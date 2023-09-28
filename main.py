6d9952abd665fc15ecef1f9296c2d740cd637f3d
Pratyush <pratyushtiwary2404@gmail.com>
2023-09-28 05:56:18 +0530

    Some logics

--- main.py
+++ main.py
@@ -4,35 +4,46 @@ import json, random
 app = Flask(__name__)
 
 
-def getLinks():
-    links = []
-    with open('static/imageData.json') as f:  
-        data = json.load(f)
-        links = data 
-    return links
 
+def getLinks():
+    accessed_indices = set()
+    with open('static/imageData.json') as f:
+        data = json.load(f)
+
+    choice = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    total_items = len(data)
+    items = []
+    
+    while len(items) < 9:
+        random_index = random.choice(choice)
+        if random_index not in accessed_indices:
+            accessed_indices.add(random_index)
+            items.append(data[random_index])
+    
+    return items
 
 
 @app.route('/')
 def landing():
-	return render_template('LandingPage.html')
+    return render_template('LandingPage.html')
 
 
 @app.route('/login')
 def login():
-	return render_template('loginPage.html')
+    return render_template('loginPage.html')
 
 
 @app.route('/contact')
 def contact():
-	return render_template('contactus.html')
+    return render_template('contactus.html')
 
 
 @app.route('/customer', methods=["GET", "POST"])
 def customer():
-	links = getLinks()
-	return render_template('customerHome.html', links = links)
+    links = getLinks()
+    return render_template('customerHome.html', links = links)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
